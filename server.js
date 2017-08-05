702c8df6978ffe2359c11a389456e45adef81f88
Scott Mitchell <smitche60@gmail.com>
2017-08-05 13:28:46 -0500

    Test button click to render data functionality

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mongoose = require('mongoose');
 var Promise = require('bluebird');
 var db = require('./server/config.js')
 var Word = require('./server/models.js')
+var startingData = require('./server/data.js')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -43,71 +44,18 @@ module.exports = app;
 
 
 
-var arr = [
-{wordValue: 'Lannister'},
-{wordValue: 'Tyrell'},
-{wordValue: 'Martell'},
-{wordValue: 'Barathean'},
-{wordValue: 'Stark'},
-{wordValue: 'Targaryen'},
-{wordValue: 'Moo'},
-{wordValue: 'LearnApp'},
-{wordValue: 'Makerpass'},
-{wordValue: 'Zoom'},
-{wordValue: 'Moo'},
-{wordValue: 'Meow'},
-{wordValue: 'Pep Talk'},
-{wordValue: 'Party Parrot'},
-{wordValue: 'Help Desk'},
-{wordValue: 'Townhall'},
-{wordValue: 'Codybot'},
-{wordValue: 'Angular'},
-{wordValue: 'React'},
-{wordValue: 'Backbone'},
-{wordValue: 'Node'},
-{wordValue: 'Squishy-Mic'},
-{wordValue: 'Greenfield'},
-{wordValue: 'Legacy'},
-{wordValue: 'Shepherd'},
-{wordValue: 'Social Hack Night'},
-{wordValue: 'BeesBeesBees'},
-{wordValue: 'SubClass Dance Party'},
-{wordValue: 'Chatterbox'},
-{wordValue: 'Precourse'},
-{wordValue: 'Thesis'},
-{wordValue: 'Shortly'},
-{wordValue: 'Sqool'},
-{wordValue: 'recast.ly'},
-{wordValue: 'HRR Cribs'},
-{wordValue: 'Tapouts'}
-]
-
-
-// Word.insertMany(arr, function(){
-//   console.log('bulk upload complete')
-// })
-
+// Initialize data in collection
 Word.deleteMany({}, function(){
-  Word.insertMany(arr, function() {
+  Word.insertMany(startingData, function() {
     Word.find({}).exec(function(err, results) {
       if (err) {
         console.log(err);
       } else {
         results.forEach(function(item){console.log(item.wordValue)})
       }
-    })
-  })
-})
-
-
-// Word.find({}).exec(function(err, word) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     // console.log(word);
-//     word.forEach(function(item){console.log(item.wordValue)})
-//   }
-// })
+    });
+  });
+});
 
 
 
--- src/components/app.js
+++ src/components/app.js
@@ -2,14 +2,20 @@ angular.module('app', [])
 
   .controller('AppController', function(){
 
-    this.sayHello = function() {
-      console.log('Hello!');
+    this.data = "I'm some data";
+
+    this.otherData = "I'm some different data"
+
+    this.renderData = function(data) {
+      this.data = data;
     }
+
 
   })
 
   .component('app', {
     controller: 'AppController',
     templateUrl: 'templates/app.html'
-    //template: "<div>I'm the app.html div!</div>"
   })
+
+
